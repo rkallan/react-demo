b7cc4f77d100f3407ae84317405ab047aeb61162
RRKallan <r.kallan@upcmail.nl>
2022-07-12 11:23:23 +0200

    type clean up

--- src/components/Hero/types/index.tsx
+++ src/components/Hero/types/index.tsx
@@ -7,15 +7,9 @@ interface InterfaceHeroProps {
     hero: TypeHeroObject;
 }
 
-interface InterfaceHeroPropsForPageHeader {
-    fullWidth?: boolean;
-    variant?: "default" | "black" | "red" | "white" | string;
-    textColor?: "default" | "black" | "red" | "white" | string;
-}
-
 type TypeHeroObject = {
     title: ReactNode;
     image: string;
 };
 
-export type { InterfaceHeroProps, InterfaceHeroPropsForPageHeader, TypeHeroObject };
+export type { InterfaceHeroProps, TypeHeroObject };
