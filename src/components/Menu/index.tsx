edd66a0bf45703661ed16e273a78056bb4224170
RRKallan <r.kallan@upcmail.nl>
2022-07-15 05:01:57 +0200

    menu fix simple keyboard support

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, MouseEvent, KeyboardEvent } from "react";
+import { useState, useEffect, useCallback, MouseEvent, KeyboardEvent, useRef, useLayoutEffect } from "react";
 import { keyEvent, getParentElementWithAttributeValue } from "@rrkallan/js-helpers";
 import { NavigationLink } from "@rrkallan/ui-library";
 import InterfaceOnOutSideClick from "./types";
@@ -6,7 +6,9 @@ import menu from "./resources/data/menu.json";
 import styles from "./resources/styles/menu.module.scss";
 
 function Menu(): JSX.Element | null {
+    const menuContainerRef = useRef<HTMLUListElement>(null);
     const [menuStateActive, setMenuStateActive] = useState(() => false);
+    const [menuItems, setMenuItems] = useState((): HTMLAnchorElement[] => []);
 
     const onKeyDownHandlerMobileButton = (event: KeyboardEvent<HTMLButtonElement>) => {
         if (event?.type === "keydown" && ![keyEvent.enter, keyEvent.spacebar].includes(event?.key)) return;
@@ -27,6 +29,25 @@ function Menu(): JSX.Element | null {
 
     const onOutSideClick: EventListener = useCallback(
         (event: InterfaceOnOutSideClick) => {
+            if (event?.type === "keydown" && [keyEvent.arrowLeft, keyEvent.arrowRight].includes(event?.key)) {
+                const lastKey = menuItems.length - 1;
+
+                menuItems.forEach((element: HTMLAnchorElement, key: number) => {
+                    if (element.isEqualNode(event.target as Node)) {
+                        let newKeyItemFocus = key;
+                        if (event.key === keyEvent.arrowLeft) newKeyItemFocus = key - 1;
+                        if (event.key === keyEvent.arrowRight) newKeyItemFocus = key + 1;
+
+                        if (newKeyItemFocus > lastKey) newKeyItemFocus = 0;
+                        if (newKeyItemFocus === -1) newKeyItemFocus = lastKey;
+
+                        menuItems[newKeyItemFocus].focus();
+                    }
+                });
+
+                return;
+            }
+
             if (event?.type === "keydown" && event?.key !== keyEvent.esc) return;
 
             const element = event?.target || undefined;
@@ -34,8 +55,19 @@ function Menu(): JSX.Element | null {
 
             if (!hasParentElementMobileButton) setMenuStateActive(!menuStateActive);
         },
-        [menuStateActive]
+        [menuItems, menuStateActive]
     );
+
+    useLayoutEffect(() => {
+        const allItems = menuContainerRef.current?.querySelectorAll("a") || [];
+        const notActiveItems: HTMLAnchorElement[] = [];
+        allItems.forEach((element: HTMLAnchorElement) => {
+            if (!element.hasAttribute("variant")) {
+                notActiveItems.push(element);
+            }
+        });
+        setMenuItems(notActiveItems);
+    }, []);
 
     useEffect(() => {
         const listenerKey = menuStateActive ? "addEventListener" : "removeEventListener";
@@ -60,7 +92,6 @@ function Menu(): JSX.Element | null {
                     onClick={onClickHandlerMobileButton}
                     onKeyDown={onKeyDownHandlerMobileButton}
                     variant="mobile-button"
-                    title=" rakkk"
                 >
                     <div className={styles.iconContainer} state={`${menuStateActive ? "is" : "in"}-active`}>
                         <span className={styles.iconUnit} />
@@ -70,7 +101,7 @@ function Menu(): JSX.Element | null {
                     </div>
                 </button>
             </div>
-            <ul className={styles.unit} state={`${menuStateActive ? "is" : "in"}-active`}>
+            <ul ref={menuContainerRef} className={styles.unit} state={`${menuStateActive ? "is" : "in"}-active`}>
                 {menu?.map(({ id, title, url, exact }) => (
                     <li key={id} className={styles.item}>
                         <NavigationLink className={styles.link} onClick={onClickHandlerNavLink} end={exact} to={url} setTabIndex>
