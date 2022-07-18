a6423ec8d11c89e6eb258b6e075b90a874609b87
RRKallan <r.kallan@upcmail.nl>
2022-07-18 03:38:20 +0200

    rebuild build

---
+++ build/static/js/WorkAssignment.6f178f00.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrrkallan_demo=self.webpackChunkrrkallan_demo||[]).push([[626],{2393:function(n,e,a){a.r(e);var l=a(3081),r=a(8895),s=a(184);const t=(0,l.ZP)((()=>a.e(528).then(a.bind(a,976))),{fallback:(0,s.jsx)(r.gb,{})});e.default=function(){return(0,s.jsx)(t,{})}}}]);
+//# sourceMappingURL=WorkAssignment.6f178f00.chunk.js.map
