1720e9df38aadcb1b36f7d313413cddc2ebfe3e2
RRKallan <r.kallan@upcmail.nl>
2022-07-28 23:50:28 +0200

    removed build folder

--- build/static/js/Error403.1817f124.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkrrkallan_demo=self.webpackChunkrrkallan_demo||[]).push([[238],{6178:function(r,e,n){n.r(e);var a=n(2791),s=n(6871),c=n(184);e.default=function(){const{pathname:r}=(0,s.TH)(),e=(0,s.s0)();return(0,a.useEffect)((()=>{"/error/403"!==r&&e("/error/403")}),[e,r]),(0,c.jsx)("div",{children:(0,c.jsx)("p",{children:"403 Error page"})})}}}]);
-//# sourceMappingURL=Error403.1817f124.chunk.js.map
